f7a8392a8e0361c7d145e2af26e86678cecba68e
mortezakhorsandnikoo <96109726+mortezakhorsandnikoo@users.noreply.github.com>
2024-04-23 23:39:30 +0330

    Update main.py

--- linearregression-class/main.py
+++ linearregression-class/main.py
@@ -10,10 +10,7 @@ from sklearn import metrics
 dataset=pd.read_csv("en.csv")
 X= np.array(dataset.drop("Y1" , axis=1))
 y=np.array(dataset[["Y1"]] )
-
 X= (X - X.mean(axis=0)) / X.std(axis=0)
-
-
 X_train, X_test, y_train , y_test=train_test_split(X, y , test_size=0.25)
 
 
@@ -22,12 +19,10 @@ def main():
     ytr=y_train
     Xts=X_test
     yts=y_test
-
-
     model= Train(X_train , y_train, ITERATION , LEARNINGRATE , STOPPINGTHRESHOLD)
     estimated_weights ,estimated_bias ,  estimated_costs=model.train()
     #print(f"the estimated_weights are {estimated_weights[-1]}\n and the estimated costs are {estimated_costs} " )
-      #predictions
+    #predictions
     y_hattr=(Xtr@estimated_weights[0])+ estimated_bias[0]
     y_hatts=(Xts@estimated_weights[0])+estimated_bias[0]
 
@@ -36,12 +31,6 @@ def main():
 
     plt.plot([i for i in range(ITERATION)] , estimated_costs)
     plt.show()
-
-
-  
-
-
-    
 
 
 if __name__=="__main__":
